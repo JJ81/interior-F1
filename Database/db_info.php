b6aedbfc0992bda729ad80a1fd2b4341fda0d449
yijaejun <yijaejun81@gmail.com>
2018-06-28 01:19:50 +0900

    관리자 추가함.

---
+++ Database/db_info.php
@@ -0,0 +1,28 @@
+<?php
+
+// Define configuration
+
+// LOCAL - LOCAL
+//define("DB_HOST", "127.0.0.1");
+//define("DB_USER", "test");
+//define("DB_PASS", "password");
+//define("DB_NAME", "php-test");
+//define("DB_PORT", "3306");
+//define("DB_CHARSET", "utf8");
+
+// LOCAL TO REAL
+//define("DB_HOST", "aknet003.cafe24.com");
+//define("DB_USER", "aknet003");
+//define("DB_PASS", "aknet3579!!01");
+//define("DB_NAME", "aknet003");
+//define("DB_PORT", "3306");
+//define("DB_CHARSET", "utf8");
+
+
+// REAL - REAL
+define("DB_HOST", "localhost");
+define("DB_USER", "aknet003");
+define("DB_PASS", "aknet3579!!01");
+define("DB_NAME", "aknet003");
+define("DB_PORT", "3306");
+define("DB_CHARSET", "utf8");
